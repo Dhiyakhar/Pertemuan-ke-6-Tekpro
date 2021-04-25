98cb8b43dea5f5999308ee87c9c70076a926ae1b
Quinque <78577301+Dhiyakhar@users.noreply.github.com>
2021-04-25 10:17:30 +0700

    Menambah Komentar pada Program

--- Exercise 3/Case 2/Manager.java
+++ Exercise 3/Case 2/Manager.java
@@ -1,28 +1,31 @@
 import java.util.Calendar; 
 import java.util.GregorianCalendar; 
 
-class Manager extends Employee implements Sortable {
+class Manager extends Employee implements Sortable {		//*Langkah 1 : ubah extends diantara "Employee" dan "Sortable" dengan implements.*//
     private String secretaryName;
-
-    public Manager(String n, double s, int d, int m, int y) {
+    
+    //Constructor
+    public Manager(String n, double s, int d, int m, int y) {	//Superclass dari Employee
         super(n, s, d, m, y);
         secretaryName = "";
     }
 
-    public void raiseSalary(double byPercent) {
+    //override methods raiseSalary yang ada pada Class Employee
+    public void raiseSalary(double byPercent) {		
         // add 1/2% bonus for every year of service
         GregorianCalendar todaysDate = new GregorianCalendar();
         int currentYear = todaysDate.get(Calendar.YEAR);
         double bonus = 0.5 * (currentYear - hireYear());
         super.raiseSalary(byPercent + bonus);
     }
-
-    public String getSecretaryName() {
+    
+    //Getter
+    public String getSecretaryName() {	//Getter secretaryName
         return secretaryName;
     }
 
 	@Override
-	public int compare(Sortable b) {
+	public int compare(Sortable b) {	//*Langkah 2 : Buat Override Sortable untuk mengurutkan.*//
 		Manager eb= (Manager) b;    
 		if (salary<eb.salary) 
 			return -1;     
