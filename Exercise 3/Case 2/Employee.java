10ab7ab98e67c95d73b9868b12cb417a334a89fa
Quinque <78577301+Dhiyakhar@users.noreply.github.com>
2021-04-25 10:18:33 +0700

    Update Employee.java

--- Exercise 3/Case 2/Employee.java
+++ Exercise 3/Case 2/Employee.java
@@ -1,10 +1,11 @@
 class Employee{
     private String name;
-    protected double salary;
+    protected double salary;	//*ubah salary dari "private" menjadi "protected"*//
     private int hireday;
     private int hiremonth;
     private int hireyear;
 
+    //constructor
     public Employee(String n, double s, int day, int month, int year) {
         name = n;
         salary = s;
@@ -12,16 +13,19 @@ class Employee{
         hiremonth = month;
         hireyear = year;
     }
-
+    
+  //Getter and setter for all the instance variables.
     public void print() {
         System.out.println(name + " " + salary + " " + hireYear());
     }
 
-    public void raiseSalary(double byPercent) {
+    //kenaikan gaji sebesar n persen
+    public void raiseSalary(double byPercent) {		
         salary *= 1 + byPercent / 100;
     }
-
-    public int hireYear() {
+    
+    //Setter tahun
+    public int hireYear() {							
         return hireyear;
     }
 }
